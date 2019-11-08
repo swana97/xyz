faa137247441b6f44828581dfb0196fc78bf7946
stc <stc@gmail.com>
2019-11-09 00:40:24 +0530

    welcome statement added

--- xyz.py
+++ xyz.py
@@ -1,1 +1,16 @@
-print("hello")
+Python 3.8.0 (tags/v3.8.0:fa919fd, Oct 14 2019, 19:21:23) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+==================== RESTART: C:\Users\swarn\Desktop\xyz.py ====================
+hello
+hi
+>>> 
+==================== RESTART: C:\Users\swarn\Desktop\xyz.py ====================
+hello
+welcome
+>>> 
+==================== RESTART: C:\Users\swarn\Desktop\xyz.py ====================
+hello
+welcome
+hi
+>>> 
